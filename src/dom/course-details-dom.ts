467bc4f3b0f9cb118ece784283cc63a5d02f565f
Yelena Kensborn <yelenakensborn@gmail.com>
2024-03-01 15:53:39 +0100

    added course details link

---
+++ src/dom/course-details-dom.ts
@@ -0,0 +1,6 @@
+import { createDiv, createParagraph, createImage, createTitle } from "./dom-modules";
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('grid-container');
+document.appendChild(gridContainer)
+
+
